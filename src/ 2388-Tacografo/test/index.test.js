e3c3f9ef20de4a7aa9e23d02e1807c6be2f5c1bf
Gabriel Mendes de Luca <gml3x7@gmail.com>
2023-11-11 17:29:43 -0300

    refactor: tests

--- src/ 2388-Tacografo/test/index.test.js
+++ src/ 2388-Tacografo/test/index.test.js
@@ -1,15 +1,15 @@
-const tacografo = require("..");
+const sut = require("..");
 path = require('path');
-
+const becrowdId = '2388';
 const cases = [
-  { input: path.resolve(__dirname, './files/2388-1.txt'), expected: 9660 },
-  { input: path.resolve(__dirname, './files/2388-2.txt'), expected: 26022 },
-  { input: path.resolve(__dirname, './files/2388-3.txt'), expected: 21205 },
+  { input: path.resolve(__dirname, `./files/${becrowdId}-1.txt`), expected: 9660 },
+  { input: path.resolve(__dirname, `./files/${becrowdId}-2.txt`), expected: 26022 },
+  { input: path.resolve(__dirname, `./files/${becrowdId}-3.txt`), expected: 21205 },
 ];
 
 describe('2388', () => {
-  test.each(cases)('tacografo-%#', ({input, expected}) => {
-    const res = tacografo(path.resolve(input));
+  test.each(cases)(`${becrowdId}-%#`, ({input, expected}) => {
+    const res = sut(path.resolve(input));
     expect(res).toBe(expected);
   });
 });
